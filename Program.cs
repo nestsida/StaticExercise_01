f0dda89f9e7afb6cd10a5f14fb582229755683ab
sarah sida <nestsida@gmail.com>
2021-10-12 13:34:38 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Static_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68); // 20
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20); //68
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celsius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
