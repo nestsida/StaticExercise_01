04a295d814dde4adaf755e9d284efd18c6cd0507
sarah sida <nestsida@gmail.com>
2021-10-12 13:56:58 -0400

    second commit

--- TempConverter.cs
+++ TempConverter.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Static_Exercise_1
 {
-    class TempConverter
+    public static class TempConverter
     {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32) / 1.8;
+
+            return result;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
     }
 }
